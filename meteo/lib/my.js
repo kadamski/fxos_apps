b468d7159effdeaca847445be83d25f11d0b3c90
Krzysztof Adamski <k@japko.eu>
2013-08-21 19:28:13 +0200

    meteo: removing. It was moved to its own repo.

--- meteo/lib/my.js
+++
@@ -1,8 +0,0 @@
-if(!String.prototype.format) {
-    String.prototype.format = function format() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, p) { 
-            return (typeof args[p] != 'undefined') ? args[p] : match; 
-        });
-    }
-}
