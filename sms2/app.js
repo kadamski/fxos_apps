4deca7144ec5b66a1f70cac8274c2ee16b52e64a
Krzysztof Adamski <k@japko.eu>
2013-08-22 20:56:13 +0200

    sms2: Display contact list with checkboxes instead of using conctact picker.

--- sms2/app.js
+++ sms2/app.js
@@ -10,9 +10,17 @@ function main() {
     function _clickSend() {
         var sms = window.navigator.mozSms;
         var text=document.getElementById('text').value;
-        var number=document.getElementById('number').value;
-        var numbers=number.split(';');
-        if(numbers.length) {
+        var con=document.getElementById('contacts').getElementsByTagName('LI');;
+        var i, cb, numbers=[];
+        for(i=0; i<con.length; i++) {
+            cb=con[i].getElementsByTagName('input')[0];
+            if(!cb.checked) {
+                continue;
+            }
+            numbers.push(cb.value);
+            console.log(cb.value);
+        }
+        if(numbers.length>0) {
             var req=sms.send(numbers, text);
             console.log(req);
             for(var i=0; i < req.length; i++) {
@@ -33,7 +41,38 @@ function main() {
         a.onerror = function() { alert("Can't pick contact list!"); };
     }
     document.getElementById('send').addEventListener('click', _clickSend);
-    document.getElementById('to').addEventListener('click', _clickTo);
+//    document.getElementById('to').addEventListener('click', _clickTo);
+    showContacts(document.getElementById('contacts'));
+}
+
+function showContacts(ul) {
+    var contacts, all, i, filter, count=0;
+
+    contacts=navigator.mozContacts;
+    request=contacts.getAll({ sortBy: 'givenName', sortOrder: 'ascending' });
+    request.onsuccess = function () {
+        var c, li, checkbox, text;
+        if(this.result) {
+            c=this.result;
+            li=document.createElement("LI");
+            checkbox=document.createElement("INPUT");
+            checkbox.type="checkbox";
+            checkbox.value=c.tel[0].value;
+            checkbox.name="tel";
+            checkbox.style.display="inline";
+            checkbox.setAttribute('data-tel', c.tel[0].value);
+            checkbox.id="tel"+(count++);
+            li.appendChild(checkbox);
+            ul.appendChild(li);
+            text=document.createTextNode([c.givenName, c.familyName].join(' '));
+            li.appendChild(text);
+            this.continue();
+        }
+    };
+    request.onerror = function (err) {
+        console.log(this.error.name);
+    };
+}
 
 }
 
